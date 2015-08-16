884f7e33fcc8878daacd17ea637c71c1246d94f7
Marchi <marchi.lau@gmail.com>
2015-08-16 15:12:06 +0800

    remove ap

--- app/controllers/google_sheets_controller.rb
+++ app/controllers/google_sheets_controller.rb
@@ -2,7 +2,7 @@ class GoogleSheetsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create_rows]
 
 	def create_rows
-		  ap row = ActiveSupport::JSON.decode(request.body.read)
+		  row = ActiveSupport::JSON.decode(request.body.read)
 		  row['abandoned_at_hkt'] = '=TEXT((INDIRECT(ADDRESS(ROW(),COLUMN()+1))*0.001)/(60*60*24)+DATE(1970,1,1)+ TIME(8,0,0),"yyyy/mm/dd hh:mm:ss")'
 		  spreadsheet_title = "[#{params[:label]}] " + params[:spreadsheet_title]
 		  worksheet_title = params[:worksheet_title]
@@ -16,7 +16,7 @@ class GoogleSheetsController < ApplicationController
 		  spreadsheet = session.spreadsheet_by_title(spreadsheet_title)
 			worksheet = spreadsheet.worksheet_by_title(worksheet_title)
 			worksheet.list.push(row)
-			ap worksheet.save
+			worksheet.save
 			render :inline => "200"
 	end
 end
